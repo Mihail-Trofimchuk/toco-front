b1a273f4ddb0ee5ab4f1777b5aa54b0d08717659
Mihail <79713435+Mihail-Trofimchuk@users.noreply.github.com>
2025-09-03 22:06:59 +0300

    fix: temp solution

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,8 +1,11 @@
 interface Window {
     ethereum?: {
       isMetaMask?: boolean;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       request: (...args: any[]) => Promise<any>;
+         // eslint-disable-next-line @typescript-eslint/no-explicit-any
       on: (eventName: string, callback: (...args: any[]) => void) => void;
+         // eslint-disable-next-line @typescript-eslint/no-explicit-any
       removeListener: (eventName: string, callback: (...args: any[]) => void) => void;
     };
   }
